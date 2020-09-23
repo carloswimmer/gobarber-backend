88c82af7a3fb54e389f4c0e276a271ae90539565
Carlos Wimmer <carloswimmer@gmail.com>
2020-09-23 19:01:15 -0300

    Implement authetication tests and hash provider

---
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,34 @@
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import AuthenticateUserService from './AuthenticateUserService';
+import CreateUserService from './CreateUserService';
+
+describe('CreateUser', () => {
+  it('should be able to authenticate', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const user = await createUser.execute({
+      email: 'joe@example.com',
+      name: 'Joe Doe',
+      password: '12341234',
+    });
+
+    const response = await authenticateUser.execute({
+      email: 'joe@example.com',
+      password: '12341234',
+    });
+
+    expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
+  });
+});
