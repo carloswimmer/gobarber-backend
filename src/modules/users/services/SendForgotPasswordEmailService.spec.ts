0612bba70adc92ebfa269df934db4b6a4eb21a51
Carlos Wimmer <carloswimmer@gmail.com>
2020-09-24 21:10:06 -0300

    Implement TDD to send forgot password email

---
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,31 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+
+describe('SendForgotPasswordEmail', () => {
+  it('should be able to recover password using email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+    );
+
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+    await fakeUsersRepository.create({
+      email: 'joe@example.com',
+      name: 'Joe Doe',
+      password: '12341234',
+    });
+
+    await sendForgotPasswordEmail.execute({
+      email: 'joe@example.com',
+    });
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+});
