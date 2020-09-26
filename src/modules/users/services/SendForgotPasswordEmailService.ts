9c7d30a5537dbd81ef21912591acdb7787841a36
Carlos Wimmer <carloswimmer@gmail.com>
2020-09-26 11:54:30 -0300

    Implement mail template

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -31,11 +31,21 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
-    await this.mailProvider.sendMail(
-      email,
-      'Pedido de recuperação de senha recebido.' +
-        `<br><strong>Token:</strong> ${token}`,
-    );
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[GoBarber] Recuperação de senha',
+      templateData: {
+        template:
+          'Olá, {{name}}. Este é o seu <strong>token</strong>: {{token}}',
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
   }
 }
 
