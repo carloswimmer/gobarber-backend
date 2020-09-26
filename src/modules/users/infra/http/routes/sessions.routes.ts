a2f3c5ff7862401b544d4bdf360efa5e5b1242a8
Carlos Wimmer <carloswimmer@gmail.com>
2020-09-25 22:25:06 -0300

    Send email on dev environment

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -2,9 +2,9 @@ import { Router } from 'express';
 
 import SessionsController from '../controllers/SessionsController';
 
-const sessionRouter = Router();
+const sessionsRouter = Router();
 const sessionsController = new SessionsController();
 
-sessionRouter.post('/', sessionsController.create);
+sessionsRouter.post('/', sessionsController.create);
 
-export default sessionRouter;
+export default sessionsRouter;
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,6 +1,5 @@
 import { inject, injectable } from 'tsyringe';
 
-// import User from '@modules/users/infra/typeorm/entities/User';
 import AppError from '@shared/errors/AppError';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
@@ -32,7 +31,7 @@ class SendForgotPasswordEmailService {
 
     await this.userTokensRepository.generate(user.id);
 
-    this.mailProvider.sendMail(
+    await this.mailProvider.sendMail(
       email,
       'Pedido de recuperação de senha recebido.',
     );
