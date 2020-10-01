3f90fb7526030e7c493ff80fc5aad04161fbb8ec
Carlos Wimmer <coiote777@gmail.com>
2020-10-01 13:57:10 -0300

    List appointments of a provider by day

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,41 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to show all appointments of a provider by day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2020, 4, 20, 16, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 20, 12, 0, 0).getTime();
+    });
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id: 'provider',
+      day: 20,
+      month: 5,
+      year: 2020,
+    });
+
+    expect(appointments).toEqual([appointment1, appointment2]);
+  });
+});
