5bc6eaac770d17ba6e8c25db60bb2c11d38aa0f1
Carlos Wimmer <coiote777@gmail.com>
2020-10-01 07:18:38 -0300

    Create routes and controllers of availability

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -17,7 +17,7 @@ type IResponse = Array<{
 @injectable()
 class ListProviderMonthAvailabilityService {
   constructor(
-    @inject('AppointemntsRepository')
+    @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
   ) {}
 
