5dc2c5843d2b629da557e4b6e738e264440aa555
Carlos Wimmer <coiote777@gmail.com>
2020-10-05 17:41:02 -0300

    Configurate cache with Redis

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -15,6 +16,9 @@ class ListProviderAppointmentsService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({
@@ -23,12 +27,20 @@ class ListProviderAppointmentsService {
     month,
     day,
   }: IRequest): Promise<Appointment[]> {
+    const cacheData = await this.cacheProvider.recover('chave_desse_valor');
+
+    console.log(cacheData);
     const appointments = this.appointmentsRepository.findAllInDayFromProvider({
       provider_id,
       year,
       month,
       day,
     });
+
+    // await this.cacheProvider.save(
+    //   'chave_desse_valor',
+    //   'Esse é o valor que eu esperava ver',
+    // );
 
     return appointments;
   }
